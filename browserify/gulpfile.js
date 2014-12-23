62b9a32d222fdfa66d566b3fb2ecd1be925e3fab
akabeko <akabekobeko@gmail.com>
2014-12-23 19:31:43 +0900

    #7 HTTP サーバー実行時のルートをプロジェクト直下に変更 src だと bower_components 以下を参照できず normalize.css がリンク切れとなるため修正

--- browserify/gulpfile.js
+++ browserify/gulpfile.js
@@ -64,7 +64,7 @@ gulp.task( 'server', [ 'watch' ], function () {
     var serveStatic = require( 'serve-static' );
 
     var app = connect();
-    app.use( serveStatic( __dirname + '/src' ) );
+    app.use( serveStatic( __dirname ) );
     app.listen( 8080 );
 } );
 
