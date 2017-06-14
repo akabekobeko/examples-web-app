ff47c1fd6099ee91f37f328d421b2c7ed2efa770
akabeko <akabekobeko@gmail.com>
2017-06-14 19:29:56 +0900

    #57 #59 Using the JavaScript Standard Style, ESLint, uglify-es

--- front-end-starter-with-gulp/src/js/App.js
+++ front-end-starter-with-gulp/src/js/App.js
@@ -1,12 +1,12 @@
-import Util from './Util.js';
+import Util from './Util.js'
 
 // Application entry point
 window.onload = () => {
-  const date = Util.formatDate();
-  console.log( '[' + date + '] Application was launched.' );
+  const date = Util.formatDate()
+  console.log('[' + date + '] Application was launched.')
 
-  const elm = document.querySelector( '.date' );
-  if( elm ) {
-    elm.textContent = date;
+  const elm = document.querySelector('.date')
+  if (elm) {
+    elm.textContent = date
   }
-};
+}
